3beb0963f4e73e55f4d41cd0ae772f0bf6ed9b42
DartSkyl <dartcyberart@gmail.com>
2024-01-25 17:43:00 +0300

    remake handler "out_of_channel"

--- handlers/chat_member_handlers.py
+++ handlers/chat_member_handlers.py
@@ -15,8 +15,8 @@ async def new_member(chat_member: ChatMember):
     Если канала платный, то сначала проверяем наличие подписки. При отсутствии подписки смотрим
     есть ли у канала пробная. Если есть, то пропускаем пользователя дальше с начислением ему пробной подписки и
     соответствующим сообщением.
-    Если пробной подписки на канале нет, то кикаем пользователя с баном на одну минуту и сообщаем ему,
-    что нужно оплатить подписку"""
+    Если пробной подписки на канале нет или он ее уже получал, то кикаем пользователя с баном на одну минуту
+    и сообщаем ему, что нужно оплатить подписку"""
     if chat_member.chat.id in channels_dict['is_paid']:
         # Запись о пользователе из таблицы канала удаляется только когда заканчивается подписка.
         # И появляется как только он оплатил подписку или выдалась пробная подписка
@@ -29,7 +29,8 @@ async def new_member(chat_member: ChatMember):
             if subscription_dict[chat_member.chat.id][0] > 0:  # Проверяем, включена ли вообще пробная подписка
 
                 # Проверяем выдавалась ли пробная подписка пользователю в этом канале
-                did_receive = await db.check_user_in_trail(user_id=chat_member.from_user.id, channel_id=chat_member.chat.id)
+                did_receive = await db.check_user_in_trail(user_id=chat_member.from_user.id,
+                                                           channel_id=chat_member.chat.id)
                 if not did_receive:
                     await SubManag.add_user_trail_sub(user_id=chat_member.from_user.id, channel_id=chat_member.chat.id)
                     await bot.send_message(chat_id=chat_member.from_user.id, text=users_mess_dict['trail_sub'])
@@ -53,20 +54,23 @@ async def new_member(chat_member: ChatMember):
         await db.new_member(channel_id=chat_member.chat.id, date_today=str(date.today()))
 
 
-@chat_member_router.chat_member(ChatMemberUpdatedFilter(member_status_changed=IS_NOT_MEMBER << MEMBER))
+@chat_member_router.chat_member(F.chat.id.in_(channels_dict['free']),
+                                ChatMemberUpdatedFilter(member_status_changed=IS_NOT_MEMBER << MEMBER))
 async def out_of_channel(chat_member: ChatMember):
-    """Хэндлер ловит тех, кто отписывается, у кого закончилась подписка или кто пытается подписаться,
-    но из-за отсутствия подписки не может (на этот случай есть заглушка, что бы лишнее не считать)"""
+    """Хэндлер ловит тех, кто отписывается от открытого канала"""
 
-    # Так как данный хэндлер активируется при всех выше описанных случаях,
-    # немного оптимизируем его, что бы искажения статистики и других ошибок
-    if chat_member.new_chat_member.status != 'kicked':
-        await db.count_out_of_channel(channel_id=chat_member.chat.id, date_today=str(date.today()))
+    await db.count_out_of_channel(channel_id=chat_member.chat.id, date_today=str(date.today()))
+
+    # Если канал платный тут несколько вариантов:
+    # 1) Закончилась подписка - бот банит
+    # 2) При попытке подписаться с отключенной у канала пробной подпиской (или уже выдавалась) - бот банит
+    # 3) Подписчик сам отписался, но подписка еще осталась - здесь считать не будем,
+    # так как при окончании подписки бот его отминусует. В первых двух случаях счетчик уже стоит
 
 
 @chat_member_router.chat_member(F.chat.id == MAIN_GROUP_ID,
                                 ChatMemberUpdatedFilter(
-                                    member_status_changed=(MEMBER | IS_NOT_MEMBER) >> ADMINISTRATOR),
+                                    member_status_changed=(MEMBER | IS_NOT_MEMBER) >> ADMINISTRATOR)
                                 )
 async def new_administrator(chat_member: ChatMember):
     """Ловим новых администраторов бота только из основной группы"""
@@ -75,7 +79,7 @@ async def new_administrator(chat_member: ChatMember):
 
 @chat_member_router.chat_member(F.chat.id == MAIN_GROUP_ID,
                                 ChatMemberUpdatedFilter(
-                                    member_status_changed=(MEMBER | IS_NOT_MEMBER) << ADMINISTRATOR),
+                                    member_status_changed=(MEMBER | IS_NOT_MEMBER) << ADMINISTRATOR)
                                 )
 async def remove_administrator(chat_member: ChatMember):
     """Удаляем администратора из списка с ID администраторов"""
